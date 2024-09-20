db8658a57d2c97c1977b37fff666acfee6ee41b5
frntml <frn.tml@gmail.com>
2024-09-20 15:43:46 -0300

    generate entitie

--- core/src/game/rooms/EnemyRoom.java
+++ core/src/game/rooms/EnemyRoom.java
@@ -3,6 +3,7 @@ package game.rooms;
 import game.GameScreen;
 import game.entities.GameEntity;
 import game.utilities.EntityClassList;
+import game.ChatScreen;
 
 import java.util.Random;
 
@@ -10,6 +11,7 @@ public abstract class EnemyRoom extends Room {
     private final EntityClassList entitiesClasses;
     private static Random random = new Random();
     private final int quantityOfEntities;
+    private boolean spawned = false;
 
     protected EnemyRoom(String mapFile, EntityClassList entitiesClasses, int quantityOfEntities) {
         super(mapFile);
@@ -20,7 +22,11 @@ public abstract class EnemyRoom extends Room {
     @Override
     public void show() {
         super.show();
-        generateEntities(quantityOfEntities);
+        while(!spawned){
+            generateEntities(quantityOfEntities);
+            spawned = true;
+        }
+
     }
 
     private void generateEntities(int quantity) {
@@ -31,14 +37,15 @@ public abstract class EnemyRoom extends Room {
     }
 
     private void generateEntity() {
-       float x=0, y=0;
-       // do{
+        ChatScreen.chat.createBig("Pene", "Texto");
+        float x=0, y=0;
+       do{
             x = random.nextFloat() * getWidth();
-       // }while(x == GameScreen.game.getPlayer().getX());
+       }while(x == GameScreen.game.getPlayer().getX());
 
-       // do{
+       do{
             y = random.nextFloat() * getHeight();
-       // }while(x == GameScreen.game.getPlayer().getY());
+       }while(x == GameScreen.game.getPlayer().getY());
 
         GameEntity entity = getRandomEntityAt(x, y);
 
