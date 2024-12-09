86c113c966c426349ad82af1f5a50bfa923c0e2f
Diego Manildo <diegothiry26@gmail.com>
2024-12-09 11:25:14 -0300

    Fixed NET room styles

--- core/src/utilities/Utils.java
+++ core/src/utilities/Utils.java
@@ -12,4 +12,18 @@ public final class Utils {
             e.printStackTrace();
         }
     }
+
+    public static int findFirstNumber(String input) {
+        StringBuilder number = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isDigit(c)) {
+                number.append(c);
+            } else if (number.length() > 0) {
+                break;
+            }
+        }
+
+        return number.length() > 0 ? Integer.parseInt(number.toString()) : -1;
+    }
 }
